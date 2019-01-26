34ace74efa9b5277d6bbc10fe62b6b180afcd805
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-26 15:39:49 -0500

    required jwt added secret variable defined login function

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('../database/dbConfig');
+
+const secret = process.env.JWT_SECRET;
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -21,12 +24,42 @@ function register(req, res) {
       res.json(user);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Unable to register new user'});
+      res.status(500).json(err);
     });
 }
 
+// function generateToken(user) {
+//   const payload = {
+//     username: user.username,
+//   };
+//
+//   const options = {
+//     expiresIn: '1h',
+//   };
+//
+//   return jwt.sign(payload, secret, options);
+// }
+
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  // console.log('user creds', creds);
+
+  db('users').where('username', creds.username)
+    .then(user => {
+      console.log('user info', user);
+      if (user.length && bcrypt.compareSync(creds.password, user[0].password)) {
+        const token = jwt.sign({
+          username: user[0].username
+        }, secret, { expiresIn: '1h'});
+        res.status(201).json({ id: user[0].id, token });
+      } else {
+        res.status(404).json({ err: 'invalid username or password'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function getJokes(req, res) {
