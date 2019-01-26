b97b450d830fd0dd0dd20549fe7bf13cd12f9dd5
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-26 14:43:50 -0500

    added morgan logger to server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('morgan');
 
 const configureRoutes = require('../config/routes.js');
 
 const server = express();
 
 server.use(helmet());
+server.use(logger('dev'));
 server.use(cors());
 server.use(express.json());
 
